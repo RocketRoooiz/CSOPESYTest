07cedb6781d3da0d97d2fe182b0d21a71331efd8
RocketRoooiz <138289829+RocketRoooiz@users.noreply.github.com>
2025-05-31 18:59:52 +0800

    Initial commit

---
+++ CSOPESYTest.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <unordered_map>
+
+using namespace std;
+
+int main(){
+
+    cout << "+==================================================+\n";
+    cout << "|          CSOPESY Test Case Upload Check          |\n";
+    cout << "+==================================================+\n";
+    cout << "Ruiz, Chester Bryan C. - CSOPESY S20";
+
+    return 0;
+}
